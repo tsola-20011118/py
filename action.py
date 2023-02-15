d6f530db1d27c5fc3bfc4063058b13c366b6d392
tsola <solatanaka2022@keio.jp>
2023-02-15 22:29:48 +0900

    0.1.1 fixed:windowSize

---
+++ action.py
@@ -0,0 +1,22 @@
+import pyxel
+
+controlSize = 16 * 6
+windowSizeX = 16 * 16
+windowSizeY = 16 * 12
+
+
+class App:
+    def __init__(self):
+        pyxel.init(windowSizeX + controlSize * 2, windowSizeY, fps=30)
+        pyxel.load("action.pyxres")
+        pyxel.run(self.update, self.draw)
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pyxel.cls(1)
+        pyxel.rect(controlSize, 0, windowSizeX, windowSizeY, 8)
+        # pyxel.blt(0,0,0,0,0,16,32, 7)
+
+App()
